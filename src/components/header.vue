651a527e5b7467405413bed5ca5a7556781c409c
Ahmed <ahmed.mostafa@code95.info>
2019-01-23 00:02:42 +0200

    init vue-router too project

---
+++ src/components/header.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+        <ul class="nav nav-tabs">
+            <router-link to="/" tag="li" active-class="active" exact>Home</router-link>
+            <router-link to="/user" tag="li" active-class="active">User</router-link>
+        </ul>
+    </div>
+</template>
+<script>
+
+</script>
+
---
+++ src/routes.js
@@ -0,0 +1,7 @@
+import User from './components/user/User'
+import Home from './components/Home'
+
+export const routes = [
+    {path: "/user", component: User},
+    {path: "/", component: Home}
+]
